9032ca2c16faad503bccf70883dc4125e5713911
Igor <i.kadziela03@gmail.com>
2026-06-15 21:53:01 +0200

    chore(types): Dodano deklarację modułu react-simple-maps dla React 19

---
+++ frontend/types/react-simple-maps.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Deklaracja modułu dla react-simple-maps (v3 nie dostarcza własnych typów
+ * kompatybilnych z React 19). Pozwala budować projekt bez @types.
+ */
+declare module "react-simple-maps";
